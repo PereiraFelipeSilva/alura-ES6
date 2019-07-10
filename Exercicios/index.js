48ce448ef525c3f3742da563b11f27a290a0b715
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-07-10 18:04:56 -0300

    fim do capítulo 2 do curso

--- Exercicios/index.js
+++ Exercicios/index.js
@@ -1,8 +1,34 @@
-let carro = {
-   velocidade: 100,
-   acelera : () =>  {
-       console.log(this);
-       console.log(`Carro a ${this.velocidade} km por hora!`);
-   }
-};
-carro.acelera();
+class Pessoa {
+
+    constructor(nome) {
+        this._nome = nome;
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    set nome(nome) {
+        this._nome = nome;
+    }
+
+    grita(frase) {
+        return `${this._nome} grita ${frase}`;
+    }
+}
+
+let pessoa = new Proxy(new Pessoa('Barney'), {
+
+        get(target, prop, receiver) {
+            if(prop == 'grita' && typeof(target[prop]) == typeof(Function)) {
+
+                return function() {
+                    console.log(`Interceptei o método: ${prop}, por isso estou exbindo essa mensagem!`);    
+                    return Reflect.apply(target[prop], target, arguments);       
+                }
+            }
+            return Reflect.get(target, prop, receiver);
+        }
+    });
+
+   console.log(pessoa.grita('Olá'));
