adfe7575f837ef406a48fbf5cfd94f5f6229593d
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-07-19 10:06:00 -0300

    implementação do padrão module pattern

--- ES6/client/js/app/services/ConnectionFactory.js
+++ ES6/client/js/app/services/ConnectionFactory.js
@@ -1,48 +1,53 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+const ConnectionFactory = (function(){
 
-class ConnectionFactory {
-
-  constructor(){
-
-    throw new Error('Esta classe não pode ser instanciada.');
+  let stores = ['negociacoes'];
+  let version = 4;
+  let dbName = 'aluraframe';
+  let connection = null;
+  
+  return class ConnectionFactory {
+  
+    constructor(){
+  
+      throw new Error('Esta classe não pode ser instanciada.');
+    };
+  
+    static getConnection(){
+  
+      return new Promise((resolve, reject) => {
+  
+        const openRequest = window.indexedDB.open(dbName, version);
+    
+        openRequest.onupgradeneeded = e => {
+    
+          ConnectionFactory._createStores(e.target.result);
+        };
+    
+        openRequest.onsuccess = e => {
+    
+          if(!connection) connection = e.target.result;
+          resolve(connection);
+        };
+    
+        openRequest.onerror = e => {
+    
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+      });
+    };
+  
+    static _createStores(connection){
+  
+      stores.forEach(store => {
+  
+        if(connection.objectStoreNames.contains(store)){
+  
+          connection.deleteObjectStore(store);
+        }
+  
+        connection.createObjectStore(store, { autoIncrement: true });
+      });
+    };
   }
-
-  static getConnection(){
-
-    return new Promise((resolve, reject) => {
-
-      const openRequest = window.indexedDB.open(dbName, version);
-  
-      openRequest.onupgradeneeded = e => {
-  
-        ConnectionFactory._createStores(e.target.result);
-      };
-  
-      openRequest.onsuccess = e => {
-  
-        resolve(e.target.result);
-      };
-  
-      openRequest.onerror = e => {
-  
-        console.log(e.target.error);
-        reject(e.target.error.name);
-      };
-    });
-  };
-
-  static _createStores(connection){
-
-    stores.forEach(store => {
-
-      if(connection.objectStoreNames.contains(store)){
-
-        connection.deleteObjectStore(store);
-      }
-
-      connection.createObjectStore(store, { autoIncrement: true });
-    });
-  };
-}
+})();
